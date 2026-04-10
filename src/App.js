cc5e7324073d7fa80f79257a488396c6401884cf
Дарья Бессонова <darinawolf0910@gmail.com>
2026-04-10 21:01:12 +0300

    Создала заголовок

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 function App() {
-  return <div>Experiment Tracker</div>;
+  const [experiments, setExperiments] = useState([]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Учёт экспериментов</h1>
+    </div>
+  );
 }
+
 export default App;
